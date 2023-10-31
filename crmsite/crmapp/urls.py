2046912f0d3980c968ca4b6b727f9ea0114c5dd6
sabrismd <90912183+sabrismd@users.noreply.github.com>
2023-10-31 12:25:48 +0530

    Add files via upload

--- crmsite/crmapp/urls.py
+++ crmsite/crmapp/urls.py
@@ -9,10 +9,13 @@ urlpatterns = [
    path('quote_v1/',views.quote_form_v1,name='q1'),
    path('quote_v2/',views.quote_form_v2,name='q2'),
    path('bill_form/',views.bill_form,name='bform'),
+   path('bill_form2/',views.bill_form2,name='bform2'),
    path('los/',views.los,name='los'),
    path('los2',views.los2,name='los2'),
    path('los3',views.los3,name='los3'),
+   path('los4',views.los4,name='los4'),
    path('vi/',views.quote,name='q'),
    path('vi2/',views.quote2,name='qv2'),
    path('vi3/',views.b,name='b1'),
+   path('vi4/',views.b2,name='b2'),
 ]
--- crmsite/crmapp/views.py
+++ crmsite/crmapp/views.py
@@ -37,6 +37,8 @@ def quote_form_v2(request):
     return render(request,'quote_form_v2.html',{})
 def bill_form(request):
     return render(request,'bill_form_v1.html',{})
+def bill_form2(request):
+    return render(request,'bill_form_v2.html',{})
 
 def los(request):
     selected_option = request.POST.get('option')
@@ -66,6 +68,15 @@ def los3(request):
         'items_to_display':items_to_display,
     }
     return render(request,'bill_form_v1.html',context=context)
+def los4(request):
+    selected_option = request.POST.get('option')
+    print(type(selected_option))
+    opt_integer = int(selected_option)
+    items_to_display = range(opt_integer)
+    context={
+        'items_to_display':items_to_display,
+    }
+    return render(request,'bill_form_v2.html',context=context)
 
 def quote(request):
     if request.method == 'POST':
@@ -195,3 +206,46 @@ def b(request):
             'd':date,'a1':a1,'a2':a2,'a3':a3,'s':sub,'b':bno,'nof':nof,'t':text,'sum':sumlist,'combined_data':combined_data,
         }
         return render(request,'preview3.html',context=context)
+    
+def b2(request):
+    if request.method == 'POST':
+        date=request.POST.get('date')
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
+        date = date_obj.strftime("%d/%m/%Y")
+        a1=request.POST.get('a1')
+        a2=request.POST.get('a2')
+        a3=request.POST.get('a3')
+        sub=request.POST.get('sub')
+        bno=request.POST.get('bno')
+        nof=request.POST.get('nof')
+        nof_int = int(nof)
+        nof_int = range(nof_int)
+        desclist=[]
+        hsnlist=[]
+        qtylist=[]
+        ratelist=[]
+        amountlist=[]
+        for i in range(len(nof_int)):
+            desclist.append(request.POST.get(f'desc{i}'))
+            hsnlist.append(request.POST.get(f'hsn{i}'))
+            qtylist.append(request.POST.get(f'qty{i}'))
+            ratelist.append(request.POST.get(f'rate{i}'))
+            amountlist.append(request.POST.get(f'amount{i}'))
+        locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+        int_amountlist = [f"{locale.atof(x):,.2f}" for x in amountlist]
+        float_values = [locale.atof(x) for x in int_amountlist]
+        scgs=[int(9/100 * j) for j in float_values]
+        sumlist = sum(float_values)
+        sumlist = locale.format_string("%.2f", sumlist, grouping=True)
+        ilist=[int(g) for g in float_values]
+        p = inflect.engine()
+        text= p.number_to_words(sumlist)
+        text=text.upper()  
+        combined_data = []
+        for a, b, c, d, e, f,g in zip(desclist, hsnlist, qtylist, ratelist,int_amountlist,scgs,ilist):
+            combined_data.append({'a': a, 'b': b, 'c': c, 'd': d, 'e': e,'f':f,'g':g,})
+
+        context={
+            'd':date,'a1':a1,'a2':a2,'a3':a3,'s':sub,'b':bno,'nof':nof,'t':text,'sum':sumlist,'combined_data':combined_data,
+        }
+        return render(request,'preview4.html',context=context)

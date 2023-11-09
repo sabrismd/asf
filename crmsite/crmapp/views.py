451c37eb3e7f9a9cc441e429d7e0a8d4de4941a2
sabrismd <90912183+sabrismd@users.noreply.github.com>
2023-11-09 15:57:37 +0530

    Add files via upload

--- crmsite/crmapp/views.py
+++ crmsite/crmapp/views.py
@@ -210,21 +210,26 @@ def b(request):
 def b2(request):
     if request.method == 'POST':
         date=request.POST.get('date')
+        pno=request.POST.get('pno')
         date_obj = datetime.strptime(date, "%Y-%m-%d")
         date = date_obj.strftime("%d/%m/%Y")
         a1=request.POST.get('a1')
         a2=request.POST.get('a2')
         a3=request.POST.get('a3')
+        a4=request.POST.get('a4')
         sub=request.POST.get('sub')
         bno=request.POST.get('bno')
         nof=request.POST.get('nof')
         nof_int = int(nof)
+        nint=int(nof)
         nof_int = range(nof_int)
         desclist=[]
         hsnlist=[]
         qtylist=[]
         ratelist=[]
         amountlist=[]
+        perlist=[9 for _ in range(1,nint+1)]
+        noflist=[ i for i in range(1,nint+1)]
         for i in range(len(nof_int)):
             desclist.append(request.POST.get(f'desc{i}'))
             hsnlist.append(request.POST.get(f'hsn{i}'))
@@ -233,19 +238,25 @@ def b2(request):
             amountlist.append(request.POST.get(f'amount{i}'))
         locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
         int_amountlist = [f"{locale.atof(x):,.2f}" for x in amountlist]
+        slist=[int(x) for x in amountlist]
         float_values = [locale.atof(x) for x in int_amountlist]
         scgs=[int(9/100 * j) for j in float_values]
+        sumscgs=sum(scgs)
         sumlist = sum(float_values)
         sumlist = locale.format_string("%.2f", sumlist, grouping=True)
         ilist=[int(g) for g in float_values]
+        at=sum(slist)
+        gt=(sumscgs*2)+at
+        at="{:,.2f}".format(at)
+        gt="{:,.2f}".format(gt)
+        sumscgs="{:,.2f}".format(sumscgs)
         p = inflect.engine()
-        text= p.number_to_words(sumlist)
-        text=text.upper()  
-        combined_data = []
-        for a, b, c, d, e, f,g in zip(desclist, hsnlist, qtylist, ratelist,int_amountlist,scgs,ilist):
-            combined_data.append({'a': a, 'b': b, 'c': c, 'd': d, 'e': e,'f':f,'g':g,})
-
+        t1=int(float(gt.replace(',','')))
+        text= p.number_to_words(t1)
+        text=text.upper()
         context={
-            'd':date,'a1':a1,'a2':a2,'a3':a3,'s':sub,'b':bno,'nof':nof,'t':text,'sum':sumlist,'combined_data':combined_data,
+            'd':date,'p':pno,'a1':a1,'a2':a2,'a3':a3,'a4':a4,'s':sub,'b':bno,'nolist':noflist,'dlist':desclist,
+            'hlist':hsnlist,'qlist':qtylist,'rlist':ratelist,'alist':amountlist,'scgs':scgs,'slist':sumlist,'tex':text,
+            'int_amountlist':int_amountlist,'plist':perlist,'sumscgs':sumscgs,'at':at,'gt':gt,
         }
         return render(request,'preview4.html',context=context)
